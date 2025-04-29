66111eadae47ba282d84a53fa228dbae03943dbf
Rolando M Cubillo III <electronikboy01@gmail.com>
2025-04-29 11:21:09 +0800

    Added a first person viewer in SampleScene
    SampleScene is my Level Design Project

--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -7,9 +7,9 @@ public class StoryManager : MonoBehaviour
     public TextMeshProUGUI KindnessValue;
     public GameObject Page1;
     public GameObject Page2;
-    public GameObject PicEnd;
-    public GameObject EvilEnd;
-    public GameObject LeaveEnd;
+    public GameObject EscapeEnding;
+    public GameObject PerishEnding;
+    public GameObject RescuedEnding;
     public int Kindness;
     // Start is called before the first frame update
     void Start()
@@ -24,49 +24,31 @@ public class StoryManager : MonoBehaviour
         
     }
 
-    public void LeavingScene()
-    {
-        Page2.SetActive(true);
-        Page1.SetActive(false);
-        Kindness = 3;
-        KindnessValue.text = Kindness.ToString();
-    }
 
-    public void Beating()
-    {
-        Kindness = Kindness - 1;
-        KindnessValue.text = Kindness.ToString();
-        if (Kindness <= 0)
-        {
-            EvilEnding();
-        }
-        //show an image that gives Manong a black eye per punch and then a bloody nose on the last
-    }
-
-    public void EvilEnding()
+    public void PerishEnd()
     {
         Page2.SetActive(false);
-        EvilEnd.SetActive(true);
+        PerishEnding.SetActive(true);
     }
 
-    public void LeaveEnding()
+    public void EscapeEnd()
     {
         Page2.SetActive(false);
-        LeaveEnd.SetActive(true);
+        EscapeEnding.SetActive(true);
     }
 
-    public void PicEnding()
+    public void RescuedEnd()
     {
         Page2.SetActive(false);
         Page1.SetActive(false);
-        PicEnd.SetActive(true);
+        RescuedEnding.SetActive(true);
     }
 
     public void Restart()
     {
         Page1.SetActive(true);
-        LeaveEnd.SetActive(false);
-        PicEnd.SetActive(false);
-        EvilEnd.SetActive(false);
+        EscapeEnding.SetActive(false);
+        RescuedEnding.SetActive(false);
+        PerishEnding.SetActive(false);
     }
 }
